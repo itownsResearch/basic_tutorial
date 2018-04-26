17e29b9de5ea484654097e969cbac119443467dd
ilokhat <lartane@gmail.com>
2018-04-26 16:58:22 +0200

    creation globe vide

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import * as itowns from 'itowns';
+
+let positionOnGlobe = { longitude: 2.351323, latitude: 48.856712, altitude: 25000000 };
+let viewerDiv = document.getElementById('viewerDiv');
+
+let globeView = new itowns.GlobeView(viewerDiv, positionOnGlobe);
