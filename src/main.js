7b5a4a53aa60bb67f538373a0c6bb5451100d3f8
ilokhat <lartane@gmail.com>
2018-04-26 17:49:32 +0200

    fond de carte stylisé

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import * as itowns from 'itowns';
+import DARK from './layers/DARK'
 
 let positionOnGlobe = { longitude: 2.351323, latitude: 48.856712, altitude: 25000000 };
 let viewerDiv = document.getElementById('viewerDiv');
 
 let globeView = new itowns.GlobeView(viewerDiv, positionOnGlobe);
+globeView.addLayer(DARK); //.then(() => true);
