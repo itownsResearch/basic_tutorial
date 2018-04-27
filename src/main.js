a75c1025a7b60f75eef6dc4b8840f6b5ba3686f9
ilokhat <lartane@gmail.com>
2018-04-27 16:22:11 +0200

    ajout menu

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import * as itowns from 'itowns';
 import DARK from './layers/DARK';
 import bati from './layers/bati';
+import parcelles_raster from './layers/parcelles_raster';
+import GuiTools from './GUI/GuiTools'
 
-let positionOnGlobe = { longitude: 2.351323, latitude: 48.856712, altitude: 2500 };
+let positionOnGlobe = { longitude: 2.46315, latitude: 48.819609, altitude: 5500 };
 let viewerDiv = document.getElementById('viewerDiv');
 
 let globeView = new itowns.GlobeView(viewerDiv, positionOnGlobe);
+
+const menuGlobe = new GuiTools('menuDiv', globeView);
 
 let layersLoaded = async function loadLayers() {
     console.log('loading dark');
@@ -14,13 +18,20 @@ let layersLoaded = async function loadLayers() {
     console.log('loading bati');
     const batitopo = await globeView.addLayer(bati).then(() => true).catch((r) => { console.error(r); return false });
     console.log('--bati done');
-    return dark && batitopo;
+    console.log('loading parcelles');
+    const parcelles = await globeView.addLayer(parcelles_raster).then(() => true).catch((r) => { console.error(r); return false });
+    console.log('--parcelles done');
+    return dark && batitopo && parcelles;
 }();
 
 globeView.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, async () => {
     console.log('globe initialized');
     if (await layersLoaded) {
         console.log('layers loaded!');
+        console.log('adding layers to menu!');
+        menuGlobe.addImageryLayersGUI(globeView.getLayers(l => l.type === 'color'));
+        menuGlobe.addGeometryLayersGUI(globeView.getLayers(l => l.type === 'geometry' && l.id != 'globe'));
+        console.log('menu completed');
     } else {
         console.error('something bad happened during layers loading..');
     }
