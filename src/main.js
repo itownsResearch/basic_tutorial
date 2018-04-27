0384174763ba8a88624bbf3bc2602e2899d6c575
ilokhat <lartane@gmail.com>
2018-04-27 12:14:16 +0200

    async await layers

--- src/main.js
+++ src/main.js
@@ -2,30 +2,27 @@ import * as itowns from 'itowns';
 import DARK from './layers/DARK';
 import bati from './layers/bati';
 
-let positionOnGlobe = { longitude: 2.351323, latitude: 48.856712, altitude: 25000000 };
+let positionOnGlobe = { longitude: 2.351323, latitude: 48.856712, altitude: 2500 };
 let viewerDiv = document.getElementById('viewerDiv');
 
 let globeView = new itowns.GlobeView(viewerDiv, positionOnGlobe);
 
-globeView.addLayer(DARK); //.then(() => true);
-globeView.addLayer(bati); //.then(() => true);
+let layersLoaded = async function loadLayers() {
+    console.log('loading dark');
+    const dark = await globeView.addLayer(DARK).then(() => true).catch((r) => { console.error(r); return false });
+    console.log('--dark done');
+    console.log('loading bati');
+    const batitopo = await globeView.addLayer(bati).then(() => true).catch((r) => { console.error(r); return false });
+    console.log('--bati done');
+    return dark && batitopo;
+}();
 
-//async function loadLayers() {
-//     console.log('loading dark');
-//     const dark = await globeView.addLayer(DARK).then(() => true).catch((r) => { console.error(r); return false });
-//     console.log('--dark done');
-//     console.log('loading bati bdtopo');
-//     const batitopo = await globeView.addLayer(bati).then(() => true).catch((r) => { console.error(r); return false });
-//     console.log('--bati done');
-//     return dark && batitopo;
-// }
-
-// let layersLoaded = loadLayers();
-// globeView.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, async () => {
-//     if (await layersLoaded) {
-//         console.log('layers loaded!');
-//     } else {
-//         console.error('something bad happened during layers loading..');
-//     }
-// });
+globeView.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, async () => {
+    console.log('globe initialized');
+    if (await layersLoaded) {
+        console.log('layers loaded!');
+    } else {
+        console.error('something bad happened during layers loading..');
+    }
+});
 
